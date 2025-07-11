e7b2893b67518d1b75638ec999b84f28d0f473fe
vellido <vellido@fktech.net>
2025-07-11 16:58:19 -0300

    eslint config changed

--- eslint.config.js
+++ eslint.config.js
@@ -19,5 +19,9 @@ export default tseslint.config([
       ecmaVersion: 2020,
       globals: globals.browser,
     },
+    rules: {
+      "no-unused-vars": "warn",
+      "@typescript-eslint/no-unused-vars": "warn",
+    },
   },
 ])
